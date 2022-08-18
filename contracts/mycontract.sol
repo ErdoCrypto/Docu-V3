912e3c8e3f3aa521ac1d73082f358d84cb3f494d
ErdoCrypto <erdocrypto@ethmail.cc>
2022-08-18 18:33:02 +0200

    test

---
+++ contracts/mycontract.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+/// @author ErdoCrypto
+/// @title Demo Documentation
+
+contract MyContract {
+    string private name;
+
+    constructor(string memory _name) {
+        name = _name;
+    }
+
+    function changeName(string memory _name) public {
+        name = _name;
+    }
+
+    function getName() public view returns (string memory) {
+        return name;
+    }
+}
